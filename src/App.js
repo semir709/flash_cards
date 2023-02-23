2750c5f6c53a8c2d6c81771c3eb531f5a5588b99
semir709 <selmansemir555@gmail.com>
2023-02-23 14:22:16 +0100

    movment of the cards

--- src/App.js
+++ src/App.js
@@ -1,21 +1,211 @@
 import "./App.css";
 import Card from "./components/Card";
 import Header from "./components/Header";
+import { motion, AnimatePresence } from "framer-motion";
+import { useEffect, useState } from "react";
+import { type } from "@testing-library/user-event/dist/type";
 
 function App() {
+  const containerVariants = {
+    default: {
+      background: "red",
+      width: 300,
+      height: 200,
+      position: "absolute",
+      top: "90%",
+      left: "20px",
+      translateY: "-90%",
+    },
+    move: {
+      top: "50%",
+      left: "50%",
+      translateY: "-50%",
+      translateX: "-50%",
+      transition: {
+        // delay: 1,
+        duration: 0.5,
+      },
+    },
+
+    exit: {
+      x: 1200,
+      opacity: 0,
+      transition: {
+        duration: 0.5,
+      },
+    },
+  };
+
+  const [index, setIndex] = useState(0);
+  const [data, setData] = useState(obj);
+
+  // console.log(index + 1, "index");
+  // console.log(data.length - 1, "data");
+
+  // useEffect(() => {
+  //   console.log(index + 1);
+  //   console.log(data.length);
+
+  //   console.log(index + 1 === data.length);
+
+  //   if (index + 1 === data.length) {
+  //     console.log("hey");
+  //     //   // const last = data[data.length - 1]; // take last element
+  //     //   // data.pop(); // remove last element
+  //     //   // setData((data) => {
+  //     //   //   let res = data[Math.floor(Math.random() * data.length - 1)];
+  //     //   //   return res;
+  //     //   // }); // randomize order of elements
+  //     //   // data.unshift(last);
+  //     //   // add last element on the beginnig of array
+  //     //   setIndex(0);
+  //   }
+  // }, [index]);
+
+  // console.log("hey");
+  // setIndex(0);
+
+  // if (typeof data[index + 1] === "undefined") {
+  //   const last = data[data.length - 1]; // take last element
+  //   data.pop(); // remove last element
+  //   // data = data[Math.floor(Math.random() * data.length - 1)]; // randomize order of elements
+  //   setData(data => {return  let d = data.unshift(last);})  // add last element on the beginnig of array
+  //   console.log(data, "data");
+  //   setIndex(0);
+  // }
+
+  // console.log(index + 1);
+  // useEffect(() => {
+  //   console.log(index + 2);
+  //   console.log(data.length);
+  //   if (index + 2 === data.length) {
+  //     setIndex(0);
+
+  //     console.log(index, "final");
+  //   }
+  // }, [index, data]);
+
+  // useEffect(() => {}, [index]);
+
+  const changeIndex = () => {
+    // console.log(index + 1);
+    console.log(data.length);
+    console.log(index + 1);
+    if (index + 1 === data.length - 1) {
+      const last = data[data.length - 1]; // take last element
+      data.pop(); // remove last element
+      data.sort(() => Math.random() - 0.5); // randomize order of elements
+
+      console.log(data);
+      data.unshift(last); // add last element on the beginnig of array
+      // console.log(data, "data");
+      setIndex(0);
+    } else {
+      // console.log("old");
+      setIndex((prev) => prev + 1);
+      // console.log(data[index + 1].word);
+      // console.log(data[index + 1]);
+    }
+  };
+
   return (
     <>
       <Header />
-      <main className="h-screen w-full relative">
-        <div
-          className="absolute top-[50%] left-[50%]
-            translate-y-[-50%] translate-x-[-50%] "
+      <main className="h-screen w-full relative ">
+        <p>{JSON.stringify(data[index + 1])}</p>
+        <motion.div
+          variants={containerVariants}
+          initial="default"
+          key={index + 1}
+          data={data[index + 1]}
         >
-          <Card />
-        </div>
+          {data[index + 1].word}
+        </motion.div>
+
+        <AnimatePresence>
+          <motion.div
+            variants={containerVariants}
+            initial="default"
+            animate="move"
+            exit="exit"
+            key={index}
+            data={data[index]}
+            onClick={changeIndex}
+          >
+            {data[index].word}
+          </motion.div>
+        </AnimatePresence>
       </main>
     </>
   );
 }
 
 export default App;
+{
+  /* <div
+          className="absolute top-[50%] left-[50%]
+            translate-y-[-50%] translate-x-[-50%] w-[500px] h-[300px]"
+        >
+          <div className="relative w-full h-full">
+            {data.map((props) => (
+              <Card data={props} key={props.id} />
+            ))}
+          </div>
+        </div> */
+}
+
+let obj = [
+  {
+    id: 1,
+    word: "Smile",
+    pronuced: "(Smajl)",
+    translate: "Osmjeh",
+    translate_info: "Smijeh, veseo",
+    rule_info: "no info",
+  },
+
+  {
+    id: 2,
+    word: "Man",
+    pronuced: "(Men)",
+    translate: "Čovjek",
+    translate_info: "Čovjek, osoba, muškarac",
+    rule_info: "Muški rod za osobu",
+  },
+
+  {
+    id: 3,
+    word: "Ball",
+    pronuced: "(Booll)",
+    translate: "Lopta",
+    translate_info: "no translate info",
+    rule_info: "za muški i ženski rod",
+  },
+
+  {
+    id: 4,
+    word: "Football",
+    pronuced: "(Futball)",
+    translate: "Nogomeeet",
+    translate_info: "no translate info",
+    rule_info: "za muški i ženski rod",
+  },
+
+  {
+    id: 5,
+    word: "House",
+    pronuced: "(Hause)",
+    translate: "Kuca",
+    translate_info: "no translate info",
+    rule_info: "za muški i ženski rod",
+  },
+
+  {
+    id: 6,
+    word: "Best",
+    pronuced: "(Best)",
+    translate: "Najbolji",
+    translate_info: "no translate info",
+    rule_info: "za muški i ženski rod",
+  },
+];
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,88 +1,77 @@
-import { useState } from "react";
-
-const data = [
-  {
-    word: "Smile",
-    pronuced: "(Smajl)",
-    translate: "Osmjeh",
-    translate_info: "Smijeh, veseo",
-    rule_info: "no info",
-  },
-
-  {
-    word: "Man",
-    pronuced: "(Men)",
-    translate: "Čovjek",
-    translate_info: "Čovjek, osoba, muškarac",
-    rule_info: "Muški rod za osobu",
-  },
-
-  {
-    word: "Ball",
-    pronuced: "(Booll)",
-    translate: "Lopta",
-    translate_info: "no translate info",
-    rule_info: "za muški i ženski rod",
-  },
-];
-
-const Card = () => {
+import { useEffect, useRef, useState } from "react";
+import { motion } from "framer-motion";
+const Card = ({ data }) => {
   const [flip, setFlip] = useState(false);
-  return (
-    <div className="w-[500px] h-[300px] [perspective:1000px;] group">
+  const [count, setCount] = useState(0);
+  const [animation, setAnimation] = useState(false);
+  const cardRef = useRef(null);
+
+  // useEffect(() => {
+  //   const card = cardRef.current;
+  //   card.classList.add("translate-x-[0]", "translate-y-[0]");
+  // }, []);
+
+  // const next = (e) => {
+  //   e.stopPropagation();
+  //   const card = cardRef.current;
+
+  //   //animation
+  //   // setAnimation(true);
+
+  //   // card.classList.add("translate-x-[300%]");
+
+  //   setFlip(false);
+  //   setCount((prev) => prev + 1);
+
+  //   // setAnimation(false);
+  // };
+
+  return <div></div>;
+};
+
+export default Card;
+
+{
+  /* <div
+      ref={cardRef}
+      className={`w-full h-full [perspective:1000px;] group transition duration-500  absolute`}
+    >
       <div
         className={` ${
           flip && "[transform:rotateY(180deg)]"
         }  relative w-full h-full
-        [transform-style:preserve-3d] transition duration-500`}
+    [transform-style:preserve-3d] transition duration-500  `}
       >
         <div
           onClick={() => setFlip(true)}
           className="w-full h-full bg-gradient-to-tl from-customPurple to-customOrange rounded-3xl p-3
-            flex flex-col justify-between items-center cursor-pointer absolute 
-            [backface-visibility:hidden]"
+        flex flex-col justify-between items-center cursor-pointer 
+        [backface-visibility:hidden] absolute"
         >
           <div className="flex flex-col items-center my-5">
-            <h2 className="text-white text-3xl mt-[40px]">
-              Lorem ipsum dolor sit amet.
-            </h2>
-            <p className="text-white text-lg mt-2">
-              Lorem ipsum dolor sit amet
-            </p>
+            <h2 className="text-white text-3xl mt-[40px]">{data.word}</h2>
+            <p className="text-white text-lg mt-2">{data.pronuced}</p>
           </div>
-          <span className="text-white ">Lorem ipsum dolor sit.</span>
+          <span className="text-white ">{data.rule_info}</span>
         </div>
 
         <div
           onClick={() => setFlip(false)}
           className="w-full h-full bg-gradient-to-tl from-customOrange to-customPurple
-            rounded-3xl p-3 flex flex-col items-center justify-between cursor-pointer absolute 
-            [transform:rotateY(180deg)] [backface-visibility:hidden]"
+        rounded-3xl p-3 flex flex-col items-center justify-between cursor-pointer
+        [transform:rotateY(180deg)] [backface-visibility:hidden] absolute "
         >
           <div className="flex flex-col items-center">
-            <h2 className="text-white text-3xl mt-[50px]">
-              Lorem ipsum dolor sit amet.
-            </h2>
-            <p className="text-white text-lg mt-2">
-              Lorem ipsum dolor sit amet
-            </p>
+            <h2 className="text-white text-3xl mt-[50px]">{data.translate}</h2>
+            <p className="text-white text-lg mt-2">{data.translate_info}</p>
           </div>
 
           <div className="ml-auto mr-5">
-            <button
-              onClick={(e) => {
-                e.stopPropagation();
-                console.log("Hey");
-              }}
-              className="text-white"
-            >
+            <button onClick={() => {}} className="text-white">
               Next
             </button>
           </div>
         </div>
       </div>
-    </div>
-  );
-};
-
-export default Card;
+    </div> */
+}
