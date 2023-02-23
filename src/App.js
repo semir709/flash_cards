726d7349fba30deedd96a9c9422874ca032b7e07
semir709 <selmansemir555@gmail.com>
2023-02-23 15:13:55 +0100

    make cards nicer

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ import { type } from "@testing-library/user-event/dist/type";
 function App() {
   const containerVariants = {
     default: {
-      background: "red",
       width: 300,
       height: 200,
       position: "absolute",
@@ -39,72 +38,15 @@ function App() {
   const [index, setIndex] = useState(0);
   const [data, setData] = useState(obj);
 
-  // console.log(index + 1, "index");
-  // console.log(data.length - 1, "data");
-
-  // useEffect(() => {
-  //   console.log(index + 1);
-  //   console.log(data.length);
-
-  //   console.log(index + 1 === data.length);
-
-  //   if (index + 1 === data.length) {
-  //     console.log("hey");
-  //     //   // const last = data[data.length - 1]; // take last element
-  //     //   // data.pop(); // remove last element
-  //     //   // setData((data) => {
-  //     //   //   let res = data[Math.floor(Math.random() * data.length - 1)];
-  //     //   //   return res;
-  //     //   // }); // randomize order of elements
-  //     //   // data.unshift(last);
-  //     //   // add last element on the beginnig of array
-  //     //   setIndex(0);
-  //   }
-  // }, [index]);
-
-  // console.log("hey");
-  // setIndex(0);
-
-  // if (typeof data[index + 1] === "undefined") {
-  //   const last = data[data.length - 1]; // take last element
-  //   data.pop(); // remove last element
-  //   // data = data[Math.floor(Math.random() * data.length - 1)]; // randomize order of elements
-  //   setData(data => {return  let d = data.unshift(last);})  // add last element on the beginnig of array
-  //   console.log(data, "data");
-  //   setIndex(0);
-  // }
-
-  // console.log(index + 1);
-  // useEffect(() => {
-  //   console.log(index + 2);
-  //   console.log(data.length);
-  //   if (index + 2 === data.length) {
-  //     setIndex(0);
-
-  //     console.log(index, "final");
-  //   }
-  // }, [index, data]);
-
-  // useEffect(() => {}, [index]);
-
   const changeIndex = () => {
-    // console.log(index + 1);
-    console.log(data.length);
-    console.log(index + 1);
     if (index + 1 === data.length - 1) {
       const last = data[data.length - 1]; // take last element
       data.pop(); // remove last element
       data.sort(() => Math.random() - 0.5); // randomize order of elements
-
-      console.log(data);
       data.unshift(last); // add last element on the beginnig of array
-      // console.log(data, "data");
       setIndex(0);
     } else {
-      // console.log("old");
       setIndex((prev) => prev + 1);
-      // console.log(data[index + 1].word);
-      // console.log(data[index + 1]);
     }
   };
 
@@ -112,14 +54,12 @@ function App() {
     <>
       <Header />
       <main className="h-screen w-full relative ">
-        <p>{JSON.stringify(data[index + 1])}</p>
         <motion.div
           variants={containerVariants}
           initial="default"
           key={index + 1}
-          data={data[index + 1]}
         >
-          {data[index + 1].word}
+          <Card data={data[index + 1]} />
         </motion.div>
 
         <AnimatePresence>
@@ -129,10 +69,9 @@ function App() {
             animate="move"
             exit="exit"
             key={index}
-            data={data[index]}
-            onClick={changeIndex}
+            // onClick={changeIndex}
           >
-            {data[index].word}
+            <Card data={data[index]} />
           </motion.div>
         </AnimatePresence>
       </main>
--- src/components/Card.js
+++ src/components/Card.js
@@ -26,7 +26,50 @@ const Card = ({ data }) => {
   //   // setAnimation(false);
   // };
 
-  return <div></div>;
+  return (
+    <div
+      ref={cardRef}
+      className={`w-full h-full [perspective:1000px;] group transition duration-500  absolute`}
+    >
+      <div
+        className={` ${
+          flip && "[transform:rotateY(180deg)]"
+        }  relative w-full h-full
+[transform-style:preserve-3d] transition duration-500  `}
+      >
+        <div
+          onClick={() => setFlip(true)}
+          className="w-full h-full bg-gradient-to-tl from-customPurple to-customOrange rounded-3xl p-3
+    flex flex-col justify-between items-center cursor-pointer 
+    [backface-visibility:hidden] absolute"
+        >
+          <div className="flex flex-col items-center my-5">
+            <h2 className="text-white text-3xl mt-[40px]">{data.word}</h2>
+            <p className="text-white text-lg mt-2">{data.pronuced}</p>
+          </div>
+          <span className="text-white ">{data.rule_info}</span>
+        </div>
+
+        <div
+          onClick={() => setFlip(false)}
+          className="w-full h-full bg-gradient-to-tl from-customOrange to-customPurple
+    rounded-3xl p-3 flex flex-col items-center justify-between cursor-pointer
+    [transform:rotateY(180deg)] [backface-visibility:hidden] absolute "
+        >
+          <div className="flex flex-col items-center">
+            <h2 className="text-white text-3xl mt-[50px]">{data.translate}</h2>
+            <p className="text-white text-lg mt-2">{data.translate_info}</p>
+          </div>
+
+          <div className="ml-auto mr-5">
+            <button onClick={() => {}} className="text-white">
+              Next
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Card;
