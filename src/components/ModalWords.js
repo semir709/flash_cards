07c689d64575bcabfdd1ed4422065eaa9d3145c7
semir709 <selmansemir555@gmail.com>
2023-02-28 14:53:51 +0100

    made modal responsive

--- src/components/ModalWords.js
+++ src/components/ModalWords.js
@@ -13,7 +13,7 @@ const ModalWords = ({
       <div className="bg-white opacity-70 fixed top-0 left-0 w-full h-full "></div>
 
       <div
-        className="bg-customPrimary py-5 px-3 rounded-lg text-white sm-w-[90%] w-[500px] min-h-[100px] max-h-[500px]
+        className="bg-customPrimary py-5 px-3 rounded-lg text-white sm:w-[500px] w-[90%] min-h-[100px] sm:max-h-[500px] max-h-fit 
         fixed top-[50%] left-[50%] -translate-x-1/2 -translate-y-1/2 flex flex-col items-center px-5"
       >
         <AiOutlineCloseCircle
@@ -39,24 +39,6 @@ const ModalWords = ({
             aspernat
           </p>
         </div>
-
-        {/* <div className="flex justify-between mb-2 ">
-          <div className="flex">
-            <span>Word:</span>
-            <p>{word}</p>
-          </div>
-          <p>{translate}</p>
-        </div> */}
-
-        {/* <div className="flex justify-between mb-2">
-          <p>{pronuced}</p>
-          <p>{grammar}</p>
-        </div>
-
-        <div className="flex justify-between mb-2">
-          <p>{rule_info}</p>
-          <p>{translate_info}</p>
-        </div> */}
       </div>
     </>
   );
