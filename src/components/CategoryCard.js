88df4180b57e41194acc29c614b133d9a134da0f
semir709 <selmansemir555@gmail.com>
2023-02-28 15:04:03 +0100

    added toggle on group words

--- src/components/CategoryCard.js
+++ src/components/CategoryCard.js
@@ -1,16 +1,30 @@
 import React, { useState } from "react";
-import { MdOutlineKeyboardArrowDown } from "react-icons/md";
+import {
+  MdOutlineKeyboardArrowDown,
+  MdOutlineKeyboardArrowUp,
+} from "react-icons/md";
 import { motion } from "framer-motion";
 import ModalWords from "./ModalWords";
 
 const CategoryCard = ({ name, takeId }) => {
+  const [toggle, setToggle] = useState(false);
+
+  const onToggle = () => {
+    if (toggle) setToggle(false);
+    else setToggle(true);
+  };
   return (
     <div className="mx-4">
-      <div className="w-full my-5 bg-customPrimary flex justify-between items-center px-5 py-1 rounded-lg text-white ">
+      <div
+        onClick={onToggle}
+        className="w-full my-5 bg-customPrimary flex justify-between
+         items-center px-5 py-1 rounded-lg text-white cursor-pointer"
+      >
         <div className="flex items-center">
           <div class="flex items-center mr-4">
             <div class="flex items-center ">
               <input
+                onClick={(e) => e.stopPropagation()}
                 type={"checkbox"}
                 class="w-4 h-4 text-blue-600 bg-gray-100
                         rounded focus:ring-transparent
@@ -22,40 +36,49 @@ const CategoryCard = ({ name, takeId }) => {
           <div className="mx-2">{name}</div>
         </div>
 
-        <div className="">
-          <MdOutlineKeyboardArrowDown
-            fontSize={30}
-            className="cursor-pointer"
-          />
+        <div className="" onClick={onToggle}>
+          {toggle ? (
+            <MdOutlineKeyboardArrowUp
+              fontSize={30}
+              className="cursor-pointer"
+            />
+          ) : (
+            <MdOutlineKeyboardArrowDown
+              fontSize={30}
+              className="cursor-pointer"
+            />
+          )}
         </div>
       </div>
 
-      <div className=" bg-customPrimary w-full py-5 px-3 rounded-lg overflow-x-auto">
-        <table className="w-full text-white table-fixed min-w-[700px]">
-          <tr className="text-left ">
-            <th className="px-2">Word</th>
-            <th>Grammar</th>
-            <th>Translate</th>
-            <th>Pronauced</th>
-          </tr>
-          {words.map(({ word, grammar, translate, pronauced, id }) => (
-            <motion.tr className="group" onClick={takeId} id={id}>
-              <td className="rounded-l-lg group-hover:bg-customHoverBgWhite cursor-pointer px-2">
-                {word}
-              </td>
-              <td className=" group-hover:bg-customHoverBgWhite cursor-pointer">
-                {grammar}
-              </td>
-              <td className=" group-hover:bg-customHoverBgWhite cursor-pointer">
-                {translate}
-              </td>
-              <td className="rounded-r-lg group-hover:bg-customHoverBgWhite cursor-pointer">
-                {pronauced}
-              </td>
-            </motion.tr>
-          ))}
-        </table>
-      </div>
+      {toggle && (
+        <div className=" bg-customPrimary w-full py-5 px-3 rounded-lg overflow-x-auto">
+          <table className="w-full text-white table-fixed min-w-[700px]">
+            <tr className="text-left ">
+              <th className="px-2">Word</th>
+              <th>Grammar</th>
+              <th>Translate</th>
+              <th>Pronauced</th>
+            </tr>
+            {words.map(({ word, grammar, translate, pronauced, id }) => (
+              <motion.tr className="group" onClick={takeId} id={id}>
+                <td className="rounded-l-lg group-hover:bg-customHoverBgWhite cursor-pointer px-2">
+                  {word}
+                </td>
+                <td className=" group-hover:bg-customHoverBgWhite cursor-pointer">
+                  {grammar}
+                </td>
+                <td className=" group-hover:bg-customHoverBgWhite cursor-pointer">
+                  {translate}
+                </td>
+                <td className="rounded-r-lg group-hover:bg-customHoverBgWhite cursor-pointer">
+                  {pronauced}
+                </td>
+              </motion.tr>
+            ))}
+          </table>
+        </div>
+      )}
     </div>
   );
 };
