6e3fc34ae24293ecb83c84eeaba32db594e6ff81
semir709 <selmansemir555@gmail.com>
2023-04-24 12:11:32 +0200

    added the randomized algorithm

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,36 +2,78 @@ import React, { useEffect, useRef, useState } from "react";
 import { client } from "../client";
 import { getData } from "../utils/data";
 
+const shuffleData = (array) => {
+  const arr = array;
+  for (let i = arr.length - 1; i > 0; i--) {
+    const random = Math.floor(Math.random() * (i + 1));
+    const temp = arr[random];
+    arr[random] = arr[i];
+    arr[i] = temp;
+  }
+
+  return arr;
+};
+
 const Home = () => {
   const [data, setData] = useState([]);
   const inputRef = useRef(null);
   const [index, setIndex] = useState(0);
   const [revile, setRevile] = useState(false);
+  const [loading, setLoading] = useState(true);
+  const [message, setMessage] = useState(null);
 
   useEffect(() => {
     const query = getData(0, 2);
     client.fetch(query).then((data) => {
       setData(data);
-      console.log(data);
+      setLoading(false);
     });
   }, []);
+
+  const checkValues = (array) => {
+    const valFirst = parseInt(array[0]);
+    const valSecond = parseInt(array[1]);
+    const valMore = array[2];
+
+    if (array[0] === "") {
+      setMessage("Please put index one and index two");
+      return false;
+    } else if (valMore) {
+      setMessage("Put only two numbers");
+      return false;
+    } else if (isNaN(valFirst)) {
+      setMessage("First index is not a number");
+      return false;
+    } else if (isNaN(valSecond)) {
+      setMessage("Second index is not a number");
+      return false;
+    } else {
+      setMessage("");
+      return { valFirst, valSecond };
+    }
+  };
 
   const sendRequest = () => {
     const values = inputRef.current.value.split(",");
 
-    const query = getData(values[0], values[1]);
+    const valObj = checkValues(values);
 
-    client.fetch(query).then((data) => {
-      setData(data);
-
-      console.log(data);
-    });
+    if (valObj !== false) {
+      const query = getData(values[0], values[1]);
+      client.fetch(query).then((data) => {
+        setData(data);
+        setLoading(false);
+      });
+    }
   };
 
   const next = () => {
     setRevile(false);
     if (index === data.length - 1) {
-      //radnomized
+      setData((prev) => {
+        const arr = shuffleData(prev);
+        return arr;
+      });
       setIndex((prev) => (prev = 0));
     } else {
       setIndex((prev) => prev + 1);
@@ -44,7 +86,7 @@ const Home = () => {
           ref={inputRef}
           className="border-2 mx-3 py-1 px-2"
           type="text"
-          placeholder="type in format: start end"
+          placeholder="startIndex,endIndex"
         />
         <button
           className="bg-gray-300 px-3 py-1"
@@ -55,8 +97,12 @@ const Home = () => {
         </button>
       </div>
 
+      {message && <p className="text-red-300">{message}</p>}
+
       <div className="flex flex-col items-center">
-        {data.length > 0 ? (
+        {loading ? (
+          <p>Loading...</p>
+        ) : data.length > 0 ? (
           <div className="bg-green-300 w-80">
             {revile ? (
               <h2>{data[index].translation}</h2>
@@ -67,6 +113,7 @@ const Home = () => {
         ) : (
           <p>NO data</p>
         )}
+
         <div className="flex w-full justify-between">
           <button type="button" onClick={() => setRevile((prev) => !prev)}>
             {revile ? "Hide" : "Show"}
